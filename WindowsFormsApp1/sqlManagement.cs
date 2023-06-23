a5560566c4c2a2a06f9a27bbb0c6e31c8a6c2405
PiruleroAnton <piruleroanton2017@gmail.com>
2023-06-23 11:58:02 +0200

    Añadida conexion a la bd funciona con extraccion de datos

--- WindowsFormsApp1/sqlManagement.cs
+++ WindowsFormsApp1/sqlManagement.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp1
 
             //SQLiteConnection sqlite_conn;
             // Create a new database connection:
-            sqlite_conn = new SQLiteConnection("Data Source=seguridad.db; Version = 3; New = True; Compress = True; ");
+            sqlite_conn = new SQLiteConnection("Data Source=c:\\Users\\oracle\\source\\repos\\WindowsFormsApp1\\WindowsFormsApp1\\seguridad.db; Version = 3; New = True; Compress = True; ");
          // Open the connection:
          try
             {
@@ -27,15 +27,37 @@ namespace WindowsFormsApp1
             }
             catch (Exception ex)
             {
+                Console.WriteLine("Error en la conexion");
+                Console.WriteLine(ex.Message + "\n"); 
+                
 
             }
             return sqlite_conn;
 
         }
-        public void buscar(SQLiteConnection conn)
-
+        public void buscar()
         {
+            SQLiteDataReader sqlite_datareader;
+            SQLiteCommand sqlite_cmd;
+            sqlite_cmd = sqlite_conn.CreateCommand();
+            sqlite_cmd.CommandText = "SELECT * FROM USUARIOS";
+            
+            try {
 
+                sqlite_datareader = sqlite_cmd.ExecuteReader();
+                while (sqlite_datareader.Read())
+                {
+                    string myreader = sqlite_datareader.GetString(1).ToString();
+                    Console.WriteLine(myreader);
+                }
+            }
+            catch (Exception ex) {
+
+                Console.WriteLine(ex.Message + "\n");
+
+            }    
+           
+            sqlite_conn.Close();
         }
     }
 }
